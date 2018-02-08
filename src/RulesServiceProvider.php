04329f13909cf43b7c0c5d95ed931d4d4e197420
Richard Tuin <richardtuin@gmail.com>
2018-02-08 19:08:09 +0100

    Introduce the rules helper

---
+++ src/RulesServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Egeniq\Laravel\Rules;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class RulesServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        require_once __DIR__.'/rules.php';
+    }
+}
